8fd9f8745bcfd8823056263aaff9ec1f89b6e4eb
Van Damage <jeannoelseneque@hotmail.com>
2025-04-10 06:54:45 +0930

    .

--- OpenGLProject/OpenGLProject/Instance.cpp
+++ OpenGLProject/OpenGLProject/Instance.cpp
@@ -2,5 +2,9 @@
 
 void Instance::Draw(Camera* camera, float windowWidth, float windowHeight, glm::vec3 ambientLight, Light* light)
 {
+    // set the shader pipeline
+    m_shader->bind();
+
+    // bind transform and other uniforms
     
 }
